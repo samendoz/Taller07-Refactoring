aee68141d1b36d3b570962a8a5fe66e79d01cc41
Petter De la Cruz <66736889+petdela@users.noreply.github.com>
2020-08-13 16:24:43 -0500

    Update InformacionAdicionalProfesor.java
    Se corrigio el smell code de data class con encapsuled field

--- TalllerRefactoring/src/modelos/InformacionAdicionalProfesor.java
+++ TalllerRefactoring/src/modelos/InformacionAdicionalProfesor.java
@@ -1,8 +1,26 @@
 package modelos;
 
 public class InformacionAdicionalProfesor {
-    public int añosdeTrabajo;
-    public String facultad;
-    public double BonoFijo;
+    private int añosdeTrabajo;
+    private String facultad;
+    private double BonoFijo;
     
+    public int getAñosdeTrabajo(){
+        return añosdeTrabajo;
+    }
+    public String getFacultad){
+        return facultad;
+    }  
+    public double getBonoFijo(){
+        return  BonoFijo;
+    } 
+    public void setAñosdeTrabajo(int año){
+        añosdeTrabajo = año;
+    } 
+    public void setFacultad(String facult){
+        facultad = facult;
+    }
+    public void setBonoFijo(double bono){
+        BonoFijo = bono;
+    }
 }
